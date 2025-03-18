e0ab37c068d212bf5f71163c9d75d3d56251cf2d
Anand6262 <pyexpert62@gmail.com>
2025-03-18 23:16:53 +0530

    changes and bug

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@
 import React from 'react';
 import { gql, useQuery } from '@apollo/client';
 
+// Define your GraphQL query using the gql template literal from @apollo/client:
+// This query requests data from the GraphQL server for a list of Pokémon.
+// It specifies exactly which fields (id, number, name, image) you want for each Pokémon.
+// The first: 10 argument limits the response to 10 Pokémon.
 const GET_POKEMONS = gql`
   query GetPokemons {
     pokemons(first: 10) {
@@ -14,6 +18,8 @@ const GET_POKEMONS = gql`
 `;
 
 const App = () => {
+
+  // Use Apollo Client’s useQuery hook to execute the GraphQL query within your React component:
   const { loading, error, data } = useQuery(GET_POKEMONS);
 
   if (loading)
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
+
+// 1) ApolloClient: Manages GraphQL requests and caching data.
+// 2) InMemoryCache: Caches GraphQL query results in memory for faster performance.
+// 3) ApolloProvider: Provides Apollo Client to your entire React app through React context.
 const client = new ApolloClient({
   uri: 'https://graphql-pokemon2.vercel.app', // Example public GraphQL API
   cache: new InMemoryCache(),
