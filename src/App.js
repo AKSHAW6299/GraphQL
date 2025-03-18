ecb093a6eec8284dca5acdf9eaf047c3d8d979cb
Anand6262 <pyexpert62@gmail.com>
2025-03-18 23:06:42 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { gql, useQuery } from '@apollo/client';
 
-function App() {
+const GET_POKEMONS = gql`
+  query GetPokemons {
+    pokemons(first: 10) {
+      id
+      number
+      name
+      image
+    }
+  }
+`;
+
+const App = () => {
+  const { loading, error, data } = useQuery(GET_POKEMONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Pokemons List:</h1>
+      {data.pokemons.map(({ id, name, image }) => (
+        <div key={id}>
+          <h3>{name}</h3>
+          <img src={image} alt={name} width={100} />
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
